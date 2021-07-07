342940d416e7b5e3b5018fd805d02a46394cd5d1
hminah0215 <hyeonminah@gmail.com>
2021-07-07 18:06:49 +0900

    [add] app.js 구성 & error페이지 작성

---
+++ NodeCat/app.js
@@ -0,0 +1,53 @@
+// NodeCat 서비스는 nodebird-api에서 api를 통해 데이터를 가져오는 것이므로
+// 사용하지 않는 미들웨어는 다 빼고 최소한으로 작성
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// index 라우터 참조
+const indexRouter = require("./routes");
+
+const app = express();
+app.set("port", process.env.PORT || 4000); // 4000번 포트 도메인 등록한 test100@a.com 아이디가 있다
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+// index
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
